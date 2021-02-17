3b30877dc134996c4b2a6a596cee5559d026bb6c
David Girard <25532812+girdav01@users.noreply.github.com>
2021-02-17 16:43:54 -0500

    Update tmconfig.py
    added new region

--- common/tmconfig.py
+++ common/tmconfig.py
@@ -14,12 +14,14 @@ United States     api.xdr.trendmicro.com
 European Union    api.eu.xdr.trendmicro.com
 Japan             api.xdr.trendmicro.co.jp
 Singapore         api.sg.xdr.trendmicro.com
-and more regions to come, Australia, Canada...
+Australia         api.au.xdr.trendmicro.com
+India             api.in.xdr.trendmicro.com
+and more regions to come...
 please update region dictionary
 '''
 region = {'us': 'https://api.xdr.trendmicro.com', 'eu': 'https://api.eu.xdr.trendmicro.com',
-          'jp': 'https://api.xdr.trendmicro.co.jp', 'sg': 'https://api.sg.xdr.trendmicro.com'}
-
+          'jp': 'https://api.xdr.trendmicro.co.jp', 'sg': 'https://api.sg.xdr.trendmicro.com',
+          'au': 'https://api.au.xdr.trendmicro.com', 'in': 'https://api.in.xdr.trendmicro.com'}
 #Addition configuration
 vt_api_key = '' # register to virustotal.com for a key
 bazaar_api_key = ''  #this is free, just register to bazaar.abuse.ch
